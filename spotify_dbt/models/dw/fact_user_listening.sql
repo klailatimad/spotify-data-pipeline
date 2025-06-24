4ca50151c6bf69b7d816f5ce55fdacf37cb3bc7c
klailatimad@gmail.com <klailatimad@gmail.com>
2025-06-24 18:59:19 -0400

    Updated dbt_project.yml with new schemas and added incremental loading to fact table

--- spotify_dbt/models/dw/fact_user_listening.sql
+++ spotify_dbt/models/dw/fact_user_listening.sql
@@ -1,16 +1,29 @@
 -- models/dw/fact_user_listening.sql
+{{ config(
+    materialized='incremental',
+    unique_key='event_id'
+) }}
+
 SELECT
-  ua.event_id,
-  ua.user_id,
-  u.country,
-  ua.track_id,
-  s.artist_name,
-  ua.timestamp,
-  ua.duration_ms,
-  ua.completed,
-  ua.skipped,
-  ua.device_type,
-  ua.location
+    ua.event_id,
+    ua.user_id,
+    ua.track_id,
+    ua.timestamp,
+    ua.duration_ms,
+    ua.completed,
+    ua.skipped,
+    ua.device_type,
+    ua.location,
+    s.track_name,
+    s.artist_name,
+    s.genre,
+    u.age,
+    u.country,
+    u.preferred_genre
 FROM {{ ref('stg_user_activity') }} ua
 JOIN {{ ref('stg_users') }} u ON ua.user_id = u.user_id
 JOIN {{ ref('stg_songs') }} s ON ua.track_id = s.track_id
+
+{% if is_incremental() %}
+WHERE ua.timestamp > (SELECT MAX(timestamp) FROM {{ this }})
+{% endif %}
